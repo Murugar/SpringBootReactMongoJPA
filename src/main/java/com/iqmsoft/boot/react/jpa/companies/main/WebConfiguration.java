a9c543708972806c8aa04629cc55d37c7a83108a
Davanon <davanon2014@gmail.com>
2017-04-10 19:09:36 +0530

    Second

--- src/main/java/com/iqmsoft/boot/react/jpa/companies/main/WebConfiguration.java
+++ src/main/java/com/iqmsoft/boot/react/jpa/companies/main/WebConfiguration.java
@@ -8,27 +8,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 @ComponentScan("com.iqmsoft.boot.react.jpa.companies.rest")
-public class WebConfiguration extends WebMvcAutoConfigurationAdapter {
+public class WebConfiguration extends WebMvcConfigurerAdapter {
 
-  @Override
-  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-    for (HttpMessageConverter<?> converter : converters) {
-      if (converter instanceof MappingJackson2HttpMessageConverter) {
-        MappingJackson2HttpMessageConverter c = (MappingJackson2HttpMessageConverter) converter;
-        ObjectMapper objectMapper = c.getObjectMapper();
-        objectMapper.setSerializationInclusion(Include.NON_NULL);
-      }
-    }
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		for (HttpMessageConverter<?> converter : converters) {
+			if (converter instanceof MappingJackson2HttpMessageConverter) {
+				MappingJackson2HttpMessageConverter c = (MappingJackson2HttpMessageConverter) converter;
+				ObjectMapper objectMapper = c.getObjectMapper();
+				objectMapper.setSerializationInclusion(Include.NON_NULL);
+			}
+		}
 
-   super.extendMessageConverters(converters);
-  }
+		super.extendMessageConverters(converters);
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		//registry.addResourceHandler("/resources/public/**").addResourceLocations("/");
+	}
 
 }
