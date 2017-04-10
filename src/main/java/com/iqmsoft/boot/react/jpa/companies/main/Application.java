e6b2e796c534bbde7ab6f58bebbf608ad9d53310
Davanon <davanon2014@gmail.com>
2017-04-10 18:53:35 +0530

    Second

--- src/main/java/com/iqmsoft/boot/react/jpa/companies/main/Application.java
+++ src/main/java/com/iqmsoft/boot/react/jpa/companies/main/Application.java
@@ -1,9 +1,11 @@
 package com.iqmsoft.boot.react.jpa.companies.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.iqmsoft.boot.react.jpa.companies")
+@EnableAutoConfiguration
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/iqmsoft/boot/react/jpa/companies/main/WebConfiguration.java
+++ src/main/java/com/iqmsoft/boot/react/jpa/companies/main/WebConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,7 +28,7 @@ public class WebConfiguration extends WebMvcAutoConfigurationAdapter {
       }
     }
 
-    super.extendMessageConverters(converters);
+   super.extendMessageConverters(converters);
   }
 
 }
